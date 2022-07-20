5e0a66d41661ce8adeeb82ce1d19fb5fad94628e
mkdlufop <mkdlufop@qq.com>
2022-07-20 17:06:29 +0800

    add: add tmux's configurations

--- install.sh
+++ install.sh
@@ -11,3 +11,18 @@ if [[ $? -eq 0 ]]; then
 	echo ""
 	echo -e "Vim's configurations is installed \e[32msuccessfully!\e[0m"
 	echo ""
+
+# install tmux's configurations
+sudo ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+if [[ $? -eq 0 ]]; then 
+	echo ""
+	echo -e "Tmux's configurations is installed \e[32msuccessfully!\e[0m"
+	echo ""
+
+# install tpm
+sudo apt install git
+mkdir -p ~/.tmux/plugins && cd ~/.tmux/plugins && git clone https://github.com/tmux-plugins/tpm
+if [[ $? -eq 0 ]]; then 
+	echo ""
+	echo -e "tpm is installed \e[32msuccessfully!\e[0m"
+	echo ""
